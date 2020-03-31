304d6c213600324280e408a0e799cccd0a1f525c
kid <kidiatoliny@gmail.com>
2020-03-31 04:48:34 -0100

    setup the basic server for dev

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+require('dotenv').config({
+	// eslint-disable-next-line no-undef
+	path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+})
+
+class App {
+	constructor() {
+		this.express = express()
+		this.middlewares()
+		this.routes()
+	}
+
+	middlewares() {
+		this.express.use(express.json())
+		this.express.use(bodyParser.json())
+		this.express.use(bodyParser.urlencoded({ extended: true }))
+	}
+
+	routes() {
+		this.express.use(require('./routes'))
+	}
+}
+
+module.exports = new App().express
+
+/* const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const app = express()
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(routes)
+module.exports = app
+*/
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+// require('./database')
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.log(`server started on port:  ${port}`)
+})
