95718e58507a347d7db13494a5224d7772c7a550
kid <kid.onestudio@gmail.com>
2020-04-01 23:11:47 -0100

    add another condition to auth middleware

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-	collectCoverage: true,
+	bail: 0,
 	collectCoverageFrom: ['src/**', '!src/database/**', '!src/tests/**'],
 	coverageDirectory: 'src/tests/coverage',
 	testEnvironment: 'node',
 	testMatch: ['**/**/tests/**/*.test.js?(x)'],
+	collectCoverage: true,
 }
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,7 +2,7 @@
  * javascript comment
  * @Author: kidiatoliny
  * @Date: 2020-04-01 04:43:42
- * @Desc:This middleware is use to prevente unauthenticated user to navigate to privates routes
+ * @Desc:This middleware is use to prevent unauthenticated user to navigate to privates routes
  */
 const jwt = require('jsonwebtoken')
 var app = require('./../../app')
@@ -11,11 +11,13 @@ module.exports = async (req, res, next) => {
 
 	if (!authHeader) return res.status(401).send({ error: 'Token not provided' })
 
-	const [, token] = authHeader.split(' ')
+	const [type, token] = authHeader.split(' ')
 	try {
-		const decode = await promisify(jwt.verify)(token, process.env.APP_SECRET)
-		req.userId = decode.id
-		return next()
+		if (type === 'Bearer') {
+			const decode = await promisify(jwt.verify)(token, process.env.APP_SECRET)
+			req.userId = decode.id
+			return next()
+		}
 	} catch (err) {
 		return res.status(401).send({ error: 'Token Invalid' })
 	}
